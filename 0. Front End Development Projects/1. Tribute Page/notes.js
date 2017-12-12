51f08ae5ffe8221be2b66cc69daa8fc8a8f20c50
Christopher Barclay <christopherjbarclay@gmail.com>
2017-12-11 21:23:23 -0800

    Create notes.js

---
+++ 0. Front End Development Projects/1. Tribute Page/notes.js
@@ -0,0 +1,11 @@
+/*
+My Tribute Page is dedicated to Chuck Shuldiner, the originator of Death Metal.
+
+"The Father of Death Metal"
+
+Link1: Chuck's Death:
+http://www.mtv.com/news/1451543/death-frontman-chuck-schuldiner-dies/
+
+Link2: Chuck's Wiki:
+https://en.wikipedia.org/wiki/Chuck_Schuldiner
+*/
